3cf7ec90ab8a697c81680f6c5e99e02c840d52a4
Lokita Mettas Yaputra <lokitamy@cs.washington.edu>
2013-03-05 15:38:37 -0800

    version2test

---
+++ Version2Test.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class Version2Test {
+
+	@Test
+	public void testVersion2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindUSCorners() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateGrid() {
+		fail("Not yet implemented");
+	}
+
+}
